6d510a3519ef4c5b56c371d6fcde5547c1ac14f4
박성춘 <songchon100@gmail.com>
2023-06-29 21:28:14 +0900

    Initial

---
+++ src/main/java/com/umc_spring/Heart_Hub/board/model/Board.java
@@ -0,0 +1,26 @@
+package com.umc_spring.Heart_Hub.board.model;
+
+import com.umc_spring.Heart_Hub.constant.entity.BaseEntity;
+import com.umc_spring.Heart_Hub.user.model.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @Column(length = 200)
+    private String content;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+}
