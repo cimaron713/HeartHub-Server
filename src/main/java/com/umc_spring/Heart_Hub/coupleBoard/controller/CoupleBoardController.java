add1e8cdf09444adfd42b710b727c7f29ec26549
Annjieun <dkslwdms2121@naver.com>
2023-07-11 20:36:51 +0900

    chore : CoupleBoard 컨트롤러 수정

--- src/main/java/com/umc_spring/Heart_Hub/coupleBoard/controller/CoupleBoardController.java
+++ src/main/java/com/umc_spring/Heart_Hub/coupleBoard/controller/CoupleBoardController.java
@@ -7,7 +7,6 @@ import com.umc_spring.Heart_Hub.coupleBoard.service.CoupleBoardService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,21 +14,28 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
+
+import static org.springframework.http.MediaType.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class CoupleBoardApiController {
+public class CoupleBoardController {
 
-    private static final Logger logger = LoggerFactory.getLogger(CoupleBoardApiController.class);
+    private static final Logger logger = LoggerFactory.getLogger(CoupleBoardController.class);
+
     private final CoupleBoardService coupleBoardService;
 
     /**
      * 게시물 작성
      */
     @PostMapping(value = "/board",
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+            consumes = {APPLICATION_JSON_VALUE, MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<ApiResponse<String>> createBoard(@RequestPart BoardDto.Request requestDto,
                                                            @RequestPart("files") BoardImageUploadDto boardImageUploadDto,
                                                            Authentication authentication) {
@@ -38,38 +44,53 @@ public class CoupleBoardApiController {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         Long postId = coupleBoardService.saveBoard(requestDto, boardImageUploadDto, userDetails.getUsername());
 
-        // return ResponseEntity.ok().body(ApiResponse.createSuccessWithNoContent("Create Success!"));
         return ResponseEntity.ok(ApiResponse.createSuccess(postId.toString(), "Create Success!"));
     }
 
     /**
      * 게시물 조회 (날짜에 따른)
      */
-    @GetMapping
+    @GetMapping("/board/search/{createAt}")
+    public ResponseEntity<List<BoardDto.Response>> getBoardsByDate(@PathVariable LocalDate createAt, Authentication authentication) {
+        List<BoardDto.Response> boardList = coupleBoardService.searchBoardList(createAt);
+        Collections.sort(boardList, Comparator.comparing(BoardDto.Response::getCreateAt));
 
+        return ResponseEntity.ok(boardList);
+    }
 
     /**
      * 게시물 수정
      */
-    @PutMapping("/board/{postId}")
-    public ResponseEntity<ApiResponse<String>> updateBoard(@PathVariable Long postId, BoardDto.Request responseDto) {
-        coupleBoardService.updateBoard(postId, responseDto);
+    @PutMapping("/board/{postId}/update")
+    public ResponseEntity<ApiResponse<String>> updateBoard(@PathVariable Long postId, @RequestBody BoardDto.Request requestDto,
+                                                           Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        BoardDto.Response result = coupleBoardService.detailBoard(postId);
+        if(!Objects.equals(result.getUserName(), userDetails.getUsername())) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body(ApiResponse.createError(HttpStatus.FORBIDDEN, "You are not allowed to update this board"));
+        }
 
-        return ResponseEntity.ok(ApiResponse.createSuccess(postId.toString(), "Create Success!"));
+        coupleBoardService.updateBoard(postId, requestDto);
+
+        return ResponseEntity.ok(ApiResponse.createSuccess(postId.toString(), "Update Success!"));
     }
 
-    @GetMapping("/{postId}/delete")
+    /**
+     * 게시물 삭제
+     */
+    @DeleteMapping("/board/{postId}/delete")
     public ResponseEntity<ApiResponse<String>> deleteBoard(@PathVariable Long postId, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         BoardDto.Response result = coupleBoardService.detailBoard(postId);
         if(!Objects.equals(result.getUserName(), userDetails.getUsername())) {
-            return ResponseEntity.status(HttpStatus.FOUND)
-                    .header(HttpHeaders.LOCATION, "/")
-                    .build();
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body(ApiResponse.createError(HttpStatus.FORBIDDEN, "You are not allowed to delete this board"));
         }
 
         coupleBoardService.deleteBoard(postId);
 
-        return ResponseEntity.ok(ApiResponse.createSuccess(postId.toString(), "Create Success!"));
+        return ResponseEntity.ok(ApiResponse.createSuccess(postId.toString(), "Delete Success!"));
     }
+
 }
