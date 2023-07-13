8b76c0838fb3b3a86ebd6049a12113651c9a89f2
Annjieun <dkslwdms2121@naver.com>
2023-07-13 15:11:28 +0900

    chore : Dto name 수정

--- src/main/java/com/umc_spring/Heart_Hub/coupleBoard/service/CoupleBoardServiceImpl.java
+++ src/main/java/com/umc_spring/Heart_Hub/coupleBoard/service/CoupleBoardServiceImpl.java
@@ -2,8 +2,8 @@ package com.umc_spring.Heart_Hub.coupleBoard.service;
 
 import com.umc_spring.Heart_Hub.constant.enums.ErrorCode;
 import com.umc_spring.Heart_Hub.constant.exception.CustomException;
-import com.umc_spring.Heart_Hub.coupleBoard.dto.BoardDto;
-import com.umc_spring.Heart_Hub.coupleBoard.dto.BoardImageUploadDto;
+import com.umc_spring.Heart_Hub.coupleBoard.dto.CoupleBoardDto;
+import com.umc_spring.Heart_Hub.coupleBoard.dto.CoupleBoardImageUploadDto;
 import com.umc_spring.Heart_Hub.coupleBoard.model.CoupleBoard;
 import com.umc_spring.Heart_Hub.coupleBoard.model.CoupleBoardImage;
 import com.umc_spring.Heart_Hub.coupleBoard.repository.CoupleBoardRepository;
@@ -35,7 +35,7 @@ public class CoupleBoardServiceImpl implements CoupleBoardService {
 
 
     @Override
-    public Long saveBoard(BoardDto.Request requestDto, BoardImageUploadDto boardImageUploadDto, String userName) {
+    public Long saveBoard(CoupleBoardDto.Request requestDto, CoupleBoardImageUploadDto boardImageUploadDto, String userName) {
         User user = userRepository.findByName(userName).orElseThrow(() -> {throw new CustomException(ErrorCode.USER_NOT_FOUND);});
 
         CoupleBoard result = CoupleBoard.builder()
@@ -60,7 +60,7 @@ public class CoupleBoardServiceImpl implements CoupleBoardService {
 
                 CoupleBoardImage img = CoupleBoardImage.builder()
                         .imgUrl("/coupleBoardImgs/" + imgFileName)
-                        .board(result)
+                        .coupleBoard(result)
                         .build();
 
                 imageRepository.save(img);
@@ -70,9 +70,9 @@ public class CoupleBoardServiceImpl implements CoupleBoardService {
     }
 
     @Override
-    public BoardDto.Response detailBoard(Long postId) {
+    public CoupleBoardDto.Response detailBoard(Long postId) {
         CoupleBoard coupleBoard = coupleBoardRepository.findById(postId).orElseThrow(() -> {throw new CustomException(ErrorCode.POST_NOT_FOUND);});
-        BoardDto.Response result = BoardDto.Response.builder()
+        CoupleBoardDto.Response result = CoupleBoardDto.Response.builder()
                 .board(coupleBoard)
                 .build();
 
@@ -80,19 +80,19 @@ public class CoupleBoardServiceImpl implements CoupleBoardService {
     }
 
     @Override
-    public List<BoardDto.Response> searchBoardList(LocalDate createAt) {
+    public List<CoupleBoardDto.Response> searchBoardList(LocalDate createAt) {
         List<CoupleBoard> boardList = coupleBoardRepository.findAllByCreatedDate(createAt);
-        List<BoardDto.Response> resultList = new ArrayList<>();
+        List<CoupleBoardDto.Response> resultList = new ArrayList<>();
 
         for(CoupleBoard board : boardList) {
-            resultList.add(new BoardDto.Response(board));
+            resultList.add(new CoupleBoardDto.Response(board));
         }
 
         return resultList;
     }
 
     @Override
-    public Long updateBoard(Long postId, BoardDto.Request requestDto) {
+    public Long updateBoard(Long postId, CoupleBoardDto.Request requestDto) {
         CoupleBoard coupleBoard = coupleBoardRepository.findById(postId).orElseThrow(() -> {throw new CustomException(ErrorCode.POST_NOT_FOUND);});
         coupleBoard.update(requestDto.getContent());
         coupleBoardRepository.save(coupleBoard);
