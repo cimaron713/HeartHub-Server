3084d75faf7a9d78778441c828e8ed826afb1c74
Annjieun <dkslwdms2121@naver.com>
2023-07-13 15:11:49 +0900

    chore : Dto name 수정

--- src/main/java/com/umc_spring/Heart_Hub/coupleBoard/service/CoupleBoardService.java
+++ src/main/java/com/umc_spring/Heart_Hub/coupleBoard/service/CoupleBoardService.java
@@ -1,7 +1,7 @@
 package com.umc_spring.Heart_Hub.coupleBoard.service;
 
-import com.umc_spring.Heart_Hub.coupleBoard.dto.BoardDto;
-import com.umc_spring.Heart_Hub.coupleBoard.dto.BoardImageUploadDto;
+import com.umc_spring.Heart_Hub.coupleBoard.dto.CoupleBoardDto;
+import com.umc_spring.Heart_Hub.coupleBoard.dto.CoupleBoardImageUploadDto;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,21 +15,21 @@ public interface CoupleBoardService {
      * @param userName 사용자 아이디
      * @return 게시물 ID
      */
-    Long saveBoard(BoardDto.Request requestDto, BoardImageUploadDto boardImageUploadDto, String userName);
+    Long saveBoard(CoupleBoardDto.Request requestDto, CoupleBoardImageUploadDto boardImageUploadDto, String userName);
 
     /**
      * 게시물 상세 조회
      * @param postId 게시물 ID
      * @return 게시물 정보
      */
-    BoardDto.Response detailBoard(Long postId);
+    CoupleBoardDto.Response detailBoard(Long postId);
 
     /**
      * 날짜에 따른 게시물 조회
      * @param createAt
      * @return
      */
-    List<BoardDto.Response> searchBoardList(LocalDate createAt);
+    List<CoupleBoardDto.Response> searchBoardList(LocalDate createAt);
 
     /**
      * 게시물 수정
@@ -37,7 +37,7 @@ public interface CoupleBoardService {
      * @param requestDto 수정 정보
      * @return 게시물 ID
      */
-    Long updateBoard(Long postId, BoardDto.Request requestDto);
+    Long updateBoard(Long postId, CoupleBoardDto.Request requestDto);
 
     /**
      * 게시물 삭제
