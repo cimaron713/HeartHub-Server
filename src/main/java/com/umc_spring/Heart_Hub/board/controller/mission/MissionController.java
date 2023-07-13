d1611abc49324a65bc3fdfb26c6d5c688e0b174b
박성춘 <songchon100@gmail.com>
2023-07-13 14:05:17 +0900

    chore : 오타 및 애너테이션 수정

--- src/main/java/com/umc_spring/Heart_Hub/board/controller/mission/MissionController.java
+++ src/main/java/com/umc_spring/Heart_Hub/board/controller/mission/MissionController.java
@@ -1,18 +1,20 @@
 package com.umc_spring.Heart_Hub.board.controller.mission;
 
 import com.umc_spring.Heart_Hub.board.dto.mission.MissionDto;
+import com.umc_spring.Heart_Hub.board.service.mission.MissionService;
 import com.umc_spring.Heart_Hub.board.service.mission.MissionServiceImpl;
 import com.umc_spring.Heart_Hub.constant.dto.ApiResponse;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class MissionController {
 
-    private MissionServiceImpl missionService;
+    private final MissionService missionService;
 
     @PostMapping("/api/mission/add")
     public ResponseEntity<ApiResponse<String>> addMissionToUser(MissionDto.MissionRequestDto missionRequestDto) {
