34bcd4040f5c1b40a189b2de324cafb7cba238dd
박성춘 <songchon100@gmail.com>
2023-07-24 01:16:42 +0900

    refactor : JWT인증 리팩토링중

--- src/main/java/com/umc_spring/Heart_Hub/constant/enums/ErrorCode.java
+++ src/main/java/com/umc_spring/Heart_Hub/constant/enums/ErrorCode.java
@@ -15,7 +15,8 @@ public enum ErrorCode {
     MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "미션 정보를 찾을 수 없습니다."),
     USER_MISSION_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "UMS 정보를 찾을 수 없습니다."),
     JWT_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인에 실패하였습니다.");
+    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인에 실패하였습니다."),
+    ALREADY_REPORTED(HttpStatus.BAD_REQUEST, "이미 신고한 사용자입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
