f33c2b61db3ef7088ea92317c7028b29d905bdde
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2023-07-24 17:47:25 +0900

    chore: boardgood 개수 얻어오기 수정

--- src/main/java/com/umc_spring/Heart_Hub/board/controller/community/BoardGoodContoller.java
+++ src/main/java/com/umc_spring/Heart_Hub/board/controller/community/BoardGoodContoller.java
@@ -6,6 +6,8 @@ import com.umc_spring.Heart_Hub.board.service.community.BoardGoodService;
 import com.umc_spring.Heart_Hub.constant.dto.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,14 +17,15 @@ public class BoardGoodContoller {
     private BoardGoodService boardGoodService;
     //좋아요 등록
     @PostMapping
-    public ResponseEntity<ApiResponse<String>> good(@RequestBody BoardDto.BoardRequestDto params){
-        boardGoodService.goodRegister(params, params.getUser().getUsername());
+    public ResponseEntity<ApiResponse<String>> good(@RequestBody BoardDto.BoardRequestDto params, Authentication authentication){
+        UserDetails userDetails = (UserDetails)authentication.getPrincipal();
+        boardGoodService.goodRegister(params, userDetails.getUsername());
         return ResponseEntity.ok().body(ApiResponse.createSuccessWithNoContent("register good success"));
     }
 
     @GetMapping("/{id}/counts")
-    public ResponseEntity<ApiResponse<Integer>> goodCount(@RequestBody BoardGoodDto.Request params){
-        int goodCnt = boardGoodService.goodCount(params.getBoardId());
+    public ResponseEntity<ApiResponse<Integer>> goodCount(@PathVariable Long id,@RequestBody BoardGoodDto.Request params){
+        int goodCnt = boardGoodService.goodCount(id);
         return ResponseEntity.ok().body(ApiResponse.createSuccess(goodCnt,"Count good success"));
     }
 
