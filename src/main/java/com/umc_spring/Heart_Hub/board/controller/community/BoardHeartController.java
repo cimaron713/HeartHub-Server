5c29309f07d81bca6a8beba03da9c908837d2f5c
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2023-07-28 17:32:29 +0900

    chore: boardHeartController 수정

--- src/main/java/com/umc_spring/Heart_Hub/board/controller/community/BoardHeartController.java
+++ src/main/java/com/umc_spring/Heart_Hub/board/controller/community/BoardHeartController.java
@@ -1,17 +1,29 @@
 package com.umc_spring.Heart_Hub.board.controller.community;
 
 
+import com.umc_spring.Heart_Hub.board.dto.community.BoardDto;
+import com.umc_spring.Heart_Hub.board.dto.community.BoardHeartDto;
 import com.umc_spring.Heart_Hub.board.service.community.BoardHeartService;
+import com.umc_spring.Heart_Hub.constant.dto.ApiResponse;
+import com.umc_spring.Heart_Hub.user.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("board/heart")
+@RequestMapping("/board")
 public class BoardHeartController {
     private BoardHeartService boardHeartService;
 
-//    @PostMapping
-//    public ResponseEntity<ApiResponse<String>> heart(@RequestBody BoardDto.BoardRequestDto params){
-//        User user =
-//    }
+    @PostMapping("/{boardId}/heart")
+    public ResponseEntity<ApiResponse<String>> heart(@PathVariable Long boardId, @RequestBody BoardHeartDto.Request params,
+                                                     Authentication authentication){
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        boardHeartService.heartRegister(params, userDetails.getUsername());
+        return ResponseEntity.ok().body(ApiResponse.createSuccessWithNoContent("Register Heart Success"));
+    }
+
+
 }
--- src/main/java/com/umc_spring/Heart_Hub/board/repository/community/BoardHeartRepository.java
+++ src/main/java/com/umc_spring/Heart_Hub/board/repository/community/BoardHeartRepository.java
@@ -1,9 +1,12 @@
 package com.umc_spring.Heart_Hub.board.repository.community;
 
 import com.umc_spring.Heart_Hub.board.model.community.Board;
+import com.umc_spring.Heart_Hub.board.model.community.BoardGood;
 import com.umc_spring.Heart_Hub.board.model.community.BoardHeart;
 import com.umc_spring.Heart_Hub.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +15,7 @@ public interface BoardHeartRepository extends JpaRepository<BoardHeart, Long> {
     Optional<BoardHeart> findByUserAndBoard(User user, Board board);
     List<BoardHeart> findAllByUserAndBoard_User(User user, User blockedUser);
     List<BoardHeart> findByUser(User user);
+
+    @Query(value = "select count(*) from BoardHeart group by BOARD_ID > :id", nativeQuery = true)
+    public List<BoardHeart> countHeart(@Param(value = "id") Long id);
 }
