0a182cc0b91fada282f9920f43c6f540663bb3eb
cimaron <cimaron713@gmail.com>
2023-08-10 15:32:24 +0900

    refactor: email 전송 내용 수정

--- src/main/java/com/umc_spring/Heart_Hub/email/impl/EmailServiceImpl.java
+++ src/main/java/com/umc_spring/Heart_Hub/email/impl/EmailServiceImpl.java
@@ -17,6 +17,8 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.SecureRandom;
+import java.util.Date;
 import java.util.Random;
 
 @Service
@@ -28,6 +30,7 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     JavaMailSender emailSender;
     public static final String code = createKey();
+    public static final String password = createPassword();
 
     public static String createKey() {
         StringBuffer key = new StringBuffer();
@@ -39,27 +42,48 @@ public class EmailServiceImpl implements EmailService {
         return key.toString();
     }
 
+    public static String createPassword() {
+        char[] charSet = new char[] {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '!', '@', '#', '$', '%', '^', '&' };
+
+        StringBuffer key = new StringBuffer();
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(new Date().getTime());
+
+        int idx = 0;
+        int len = charSet.length;
+        for (int i=0; i<8; i++) {
+            idx = sr.nextInt(len);
+            key.append(charSet[idx]);
+        }
+
+        return key.toString();
+    }
     public MimeMessage createVerificationMessage(String to) throws Exception{
         LOGGER.info("[createMessage] 보내는 대상 {}, 인증 번호 {}", to, code);
         MimeMessage message = emailSender.createMimeMessage();
         message.addRecipients(MimeMessage.RecipientType.TO, to);
-        message.setSubject("이메일 인증 코드 입니다.");
+        message.setSubject("[HEART HUB] 인증번호 안내");
         String msgg="";
         msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> 이메일 진증 코드 입니다.. </h1>";
+        msgg+= "<h1> 안녕하세요.HEART HUB 입니다. 회원님의 가입을 환영합니다! </h1>";
         msgg+= "<br>";
-        msgg+= "<p>아래 코드를 복사해 입력해주세요</p>";
+        msgg+= "<p>아래의 인증번호를 복사하신 후 이메일 인증번호 입력란에 입력해 주시기 바랍니다. </p>";
         msgg+= "<br>";
-        msgg+= "<p>테스트.</p>";
+        msgg+= "<p>인증번호는 발송된 시점부터 ;<span style=\"color: rgb(0, 0, 255);\">5분간만</span>; 유효하니 확인 후 바로 입력해 주시기 바랍니다.</p>";
         msgg+= "<br>";
         msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
         msgg+= "<h3 style='color:blue;'>이메일 인증 코드 입니다</h3>";
         msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
+        msgg+= "인증번호 : <strong>";
         msgg+= code+"</strong><div><br/> ";
+        msgg+= "<p> 감사합니다. </p>";
         msgg+= "</div>";
         message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("jinuktest208@gmail.com","test"));
+        message.setFrom(new InternetAddress("jinuktest208@gmail.com","HEARTHUB"));
         return message;
     }
 
@@ -67,46 +91,41 @@ public class EmailServiceImpl implements EmailService {
         LOGGER.info("[createMessage] 보내는 대상 {}, 아이디 {}", to, username);
         MimeMessage  message = emailSender.createMimeMessage();
         message.addRecipients(MimeMessage.RecipientType.TO, to);
-        message.setSubject("아이디 입니다.");
+        message.setSubject("[HEART HUB] 아이디 발급");
         String msgg="";
         msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> 아이디 입니다.. </h1>";
+        msgg+= "<p>귀하의 귀하의 아이디는 <strong>";
+        msgg+= username + "</strong>입니다.</p>";
         msgg+= "<br>";
+        msgg+= "<p>귀하께서 요청하신 내용이 아니라면 회원님의 소중한 계정 정보 보호를 위해 계정의 비밀번호를 &apos;즉시&apos; 변경해 주세요.</p>";
         msgg+= "<br>";
-        msgg+= "<p>테스트.</p>";
+        msgg+= "<p>추가 문의가 필요하신 경우 haeun9492@hub.com으로 메일을 보내주세요. 감사합니다.</p>";
         msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>아이디 입니다</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= username+"</strong><div><br/> ";
+        msgg+= "<p>*이 메일은 발신전용이므로 회신되지 않습니다.</p>";
         msgg+= "</div>";
         message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("jinuktest208@gmail.com","test"));
+        message.setFrom(new InternetAddress("jinuktest208@gmail.com","HEARTHUB"));
         return message;
     }
 
     public MimeMessage createTemporaryPasswdMessage(String to) throws Exception{
-        LOGGER.info("[createMessage] 보내는 대상 {}, 인증 번호 {}", to, code);
+        LOGGER.info("[createMessage] 보내는 대상 {}, 임시비밀번호 {}", to, password);
         MimeMessage message = emailSender.createMimeMessage();
         message.addRecipients(MimeMessage.RecipientType.TO, to);
-        message.setSubject("임시 비밀번호 입니다.");
+        message.setSubject("[HEART HUB] 임시 비밀번호 발급");
         String msgg="";
         msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> 임시 비밀번호 입니다.. </h1>";
+        msgg+= "<p>귀하의 임시 비밀번호는 <strong>";
+        msgg+= password + "</strong>입니다.  로그인 후 반드시 비밀번호를 변경해 주시길 바랍니다.</p>";
         msgg+= "<br>";
-        msgg+= "<p>아래 코드를 복사해 입력해주세요</p>";
+        msgg+= "<p>귀하께서 요청하신 내용이 아니라면 회원님의 소중한 계정 정보 보호를 위해 계정의 비밀번호를 &apos;즉시&apos; 변경해 주세요.</p>";
         msgg+= "<br>";
-        msgg+= "<p>테스트.</p>";
+        msgg+= "<p>추가 문의가 필요하신 경우 haeun9492@hub.com으로 메일을 보내주세요. 감사합니다.</p>";
         msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>임시 비밀번호 입니다</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= code+"</strong><div><br/> ";
+        msgg+= "<p>*이 메일은 발신전용이므로 회신되지 않습니다.</p>";
         msgg+= "</div>";
         message.setText(msgg, "utf-8", "html");//내용
-        message.setFrom(new InternetAddress("jinuktest208@gmail.com","test"));
+        message.setFrom(new InternetAddress("jinuktest208@gmail.com","HEARTHUB"));
         return message;
     }
 
@@ -127,7 +146,7 @@ public class EmailServiceImpl implements EmailService {
         msgg+= "<div style='margin:20px;'>";
         msgg+= "</div>";
         message.setText(msgg, "utf-8", "html");
-        message.setFrom(new InternetAddress("jinuktest208@gmail.com","test"));
+        message.setFrom(new InternetAddress("jinuktest208@gmail.com","HEARTHUB"));
         return message;
     }
 
@@ -148,7 +167,7 @@ public class EmailServiceImpl implements EmailService {
         msgg+= "<div style='margin:20px;'>";
         msgg+= "</div>";
         message.setText(msgg, "utf-8", "html");
-        message.setFrom(new InternetAddress("jinuktest208@gmail.com","test"));
+        message.setFrom(new InternetAddress("jinuktest208@gmail.com","HEARTHUB"));
         return message;
     }
 
@@ -169,7 +188,7 @@ public class EmailServiceImpl implements EmailService {
         msgg+= "<div style='margin:20px;'>";
         msgg+= "</div>";
         message.setText(msgg, "utf-8", "html");
-        message.setFrom(new InternetAddress("jinuktest208@gmail.com","test"));
+        message.setFrom(new InternetAddress("jinuktest208@gmail.com","HEARTHUB"));
         return message;
     }
 
@@ -203,7 +222,7 @@ public class EmailServiceImpl implements EmailService {
             es.printStackTrace();
             throw new IllegalArgumentException();
         }
-        return code;
+        return password;
     }
 
     @Override
